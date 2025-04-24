cb9b1f52dacf528b03d729a2343ad9943fb8c577
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-04-24 20:19:14 +0100

    Add secret key in env.py and set in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c_mjpg_7qo49-rcn8!#2s53%&u*1@#%p(ul*ih_5q8hd(-%!hp'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
